47e80e6b3428350a14073179ba738fab11791ca0
Guillermo Theler <guillermot@onesolutions.com.ar>
2024-06-29 11:23:32 -0300

    compileAll script

--- compileAll.sh
+++ compileAll.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-cd /home/prog/java_mvn/ppbridge/ppapi
+cd webapps-commons
+mvn clean install
+cd ..
 
-GCEMPOS_JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-JAVA_HOME=$GCEMPOS_JAVA_HOME; mvn clean install
-
-cd /home/prog/java_mvn/ppbridge/pprestclient
-
-GCEMPOS_JAVA_HOME=/usr/java/default1_8/
-
-JAVA_HOME=$GCEMPOS_JAVA_HOME; mvn clean install
-
-cd /home/prog/java_mvn/ppbridge/ppbridge
-
-GCEMPOS_JAVA_HOME=/usr/java/default1_8/
-
-JAVA_HOME=$GCEMPOS_JAVA_HOME; mvn clean install
-
-cd /home/prog/java_mvn/ppbridge
+cd pf-commons
+mvn clean install
+cd ..
