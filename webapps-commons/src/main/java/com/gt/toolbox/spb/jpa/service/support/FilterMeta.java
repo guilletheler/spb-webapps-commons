dc4dd68806754ab02899a3ecc58e7ad2539c5173
Guillermo Theler <guillermot@onesolutions.com.ar>
2025-08-13 07:47:23 -0300

    mejoras QueryHelper

--- webapps-commons/src/main/java/com/gt/toolbox/spb/jpa/service/support/FilterMeta.java
+++ webapps-commons/src/main/java/com/gt/toolbox/spb/jpa/service/support/FilterMeta.java
@@ -1,11 +1,13 @@
 package com.gt.toolbox.spb.jpa.service.support;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
-
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -20,11 +22,18 @@ public class FilterMeta implements Serializable {
     private String value;
 
     /**
-     * en caso que childrens no sea null o de largo 0, los operadores pueden ser
-     * AND, OR, NOT
-     * caso contrario es Operador SQL, por defecto like para string, = para el resto
+     * en caso que childrens no sea null o de largo 0, los operadores pueden ser AND, OR, NOT caso
+     * contrario es Operador SQL, por defecto like para string, = para el resto
      */
     private String operator;
 
+    @Getter(value = AccessLevel.NONE)
     private List<FilterMeta> childrens;
+
+    public List<FilterMeta> getChildrens() {
+        if (childrens == null) {
+            childrens = new ArrayList<>();
+        }
+        return childrens;
+    }
 }
